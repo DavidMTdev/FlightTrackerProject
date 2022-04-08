2d6eed7f2a109eb4ef68dde0c63d6d04711c0699
DavidMTdev <david.teixeira31@gmail.com>
2022-04-08 11:30:15 +0200

    Add init api

---
+++ flighttracker/src/main/java/com/flighttracker/api/flighttracker/FlighttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.flighttracker.api.flighttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlighttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlighttrackerApplication.class, args);
+	}
+
+}
