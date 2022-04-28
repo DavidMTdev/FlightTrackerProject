c12f13aad4f2a42e24726c8da1f73a46cfc24dbc
VENOCKA <fantshark@gmail.com>
2022-04-28 09:41:48 +0200

    FlightController Done

--- flighttracker/src/main/java/com/flighttracker/api/services/FlightService.java
+++ flighttracker/src/main/java/com/flighttracker/api/services/FlightService.java
@@ -1,10 +1,16 @@
 package com.flighttracker.api.services;
 
+import com.flighttracker.api.entities.Aircraft;
 import com.flighttracker.api.entities.Flight;
+import com.flighttracker.api.models.FlightR;
 import com.flighttracker.api.repositories.FlightRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FlightService {
@@ -17,6 +23,17 @@ public class FlightService {
 
         if (f == null) return flightRepository.save(flight);
         return f;
+    }
+
+    public List<Flight> AllFlight(){
+
+        return flightRepository.findAll();
+    }
+
+    public List<Flight> FlightById(long id){
+        Optional<Flight> f = flightRepository.findById(id);
+
+        return f.stream().collect(Collectors.toList());
     }
     
 }
